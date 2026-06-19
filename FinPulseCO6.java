5f9f8b8f8daebbf0bd1e347c9967bef252497c2a
sudheer-babu07 <kotasudheerbabu@klh.edu.in>
2026-06-19 12:40:57 +0530

    Add files via upload

---
+++ FinPulseCO6.java
@@ -0,0 +1,32 @@
+public class FinPulseCO6 {
+
+    static int maxFraudValue(int arr[]) {
+
+        int n = arr.length;
+
+        if (n == 0) return 0;
+        if (n == 1) return arr[0];
+
+        int dp[] = new int[n];
+
+        dp[0] = arr[0];
+        dp[1] = Math.max(arr[0], arr[1]);
+
+        for (int i = 2; i < n; i++) {
+            dp[i] = Math.max(dp[i - 1],
+                    dp[i - 2] + arr[i]);
+        }
+
+        return dp[n - 1];
+    }
+
+    public static void main(String[] args) {
+
+        int transactions[] =
+                {5000, 12000, 7000, 15000, 9000};
+
+        System.out.println(
+                "Maximum Suspicious Value = "
+                + maxFraudValue(transactions));
+    }
+}
